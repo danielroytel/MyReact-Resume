d9a5b4c913c30e47908dcd6093d66aa47f333eff
danielroytel <danielroytel@gmail.com>
2023-01-05 21:16:15 +1100

    update git ignore | added correct website url

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Daniel Roytel',
     description:
       'Daniel Roytel is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://danielroytel.com.au', // No trailing slash allowed!
+    siteUrl: 'https://danielroytel.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
      twitterUsername: '@',
   },
@@ -21,6 +21,7 @@ module.exports = {
       resolve: `gatsby-plugin-s3`,
       options: {
         bucketName: "danielroytel",
+        acl: null
       },
     },
     {
