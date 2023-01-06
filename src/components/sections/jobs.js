53e016b03f4c5376eb77dbdbf451e4785f739252
danielroytel <danielroytel@gmail.com>
2023-01-06 17:39:10 +1100

    Patched Sizing for Phones (Jobs Slider)

--- src/components/sections/jobs.js
+++ src/components/sections/jobs.js
@@ -82,15 +82,18 @@ const StyledTabButton = styled.button`
   white-space: nowrap;
 
   @media (max-width: 768px) {
+    min-width: 180px;
     padding: 0 15px 2px;
+    white-space: wrap;
   }
   @media (max-width: 600px) {
     ${({ theme }) => theme.mixins.flexCenter};
-    min-width: 120px;
+    min-width: 180px;
     padding: 0 15px;
     border-left: 0;
     border-bottom: 2px solid var(--lightest-navy);
     text-align: center;
+    white-space: wrap;
   }
 
   &:hover,
@@ -132,6 +135,7 @@ const StyledTabPanels = styled.div`
   margin-left: 20px;
 
   @media (max-width: 600px) {
+    min-width: 180px;
     margin-left: 0;
   }
 `;
