66f64a74fe3ae4d6a67d965b5a0d594688e2777a
danielroytel <danielroytel@gmail.com>
2023-01-07 12:21:02 +1100

    Updated Site Description and added new og.png file

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Daniel Roytel',
     description:
-      'Daniel Roytel is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Daniel Roytel is a Data Engineering Professional who specializes in building Big Data Solutions',
     siteUrl: 'https://danielroytel.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
      twitterUsername: '@',
